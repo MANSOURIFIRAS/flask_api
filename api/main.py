351044670deea94413dd6658b92bdaf61dbe4186
MANSOURIFIRAS <mansouri.firas@esprit.tn>
2023-05-08 04:36:01 +0200

    change main.py

--- api/main.py
+++ api/main.py
@@ -1,8 +1,9 @@
-import os
-import pickle
-import numpy as np
-from PIL import Image
-from flask import Flask , jsonify, request
+# import os
+# import pickle
+# import numpy as np
+# from PIL import Image
+from flask import Flask 
+# , jsonify, request
 
 app = Flask(__name__)
 
